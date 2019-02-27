4ae1c320e511ccca20475666266f1bd13a0b35cb
BidGem <pietro.peppo@gmail.com>
2019-02-27 19:09:40 +0100

    xml e AdMob

--- app/src/main/java/com/example/voicetuner/MainActivity.java
+++ app/src/main/java/com/example/voicetuner/MainActivity.java
@@ -9,16 +9,21 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends Activity {
 
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
     private final int RECORD_AUDIO_REQ_COD = 1010;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        loadAds();
         ActivityCompat.requestPermissions(this, permissions, RECORD_AUDIO_REQ_COD);
     }
 
@@ -34,5 +39,14 @@ public class MainActivity extends Activity {
         if (!permissionToRecordGranted) {
             Toast.makeText(getApplicationContext(), "L'app non può funzionare senza permessi. Sto uscendo...", Toast.LENGTH_LONG);
             finish();}
+    }
+
+    private void loadAds() {
+        MobileAds.initialize(this, "ca-app-pub-8343076377545122~2378764836");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("720C771F4D03D985B863C6D49FAE708A")  //TODO: solo per il test, rimuovere questa stringa al lancio
+                .build();
+        mAdView.loadAd(adRequest);
     }
 }
