cd0a105d71a864cb11555b67cc203025dc5c3486
BidGem <pietro.peppo@gmail.com>
2019-03-25 00:01:51 +0100

    OS Libraries attribuition

--- app/src/main/java/com/truebeans/voicetuner/activity/OpenSourceActivity.java
+++ app/src/main/java/com/truebeans/voicetuner/activity/OpenSourceActivity.java
@@ -1,7 +1,10 @@
 package com.truebeans.voicetuner.activity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.truebeans.voicetuner.R;
 
@@ -12,7 +15,9 @@ public class OpenSourceActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_source);
         initToolBar();
+        initTextViews();
     }
 
     private void initToolBar() {
@@ -27,4 +32,20 @@ public class OpenSourceActivity extends AppCompatActivity {
         finish();
         return true;
     }
+
+    @SuppressLint("SetTextI18n")
+    private void initTextViews() {
+        TextView graphViewLicense = findViewById(R.id.graphviewLicense);
+        graphViewLicense.setText(getString(R.string.license_header1) + " " + getString(R.string.graphview) + getString(R.string.license_header2)
+                + getString(R.string.graphView_license));
+
+        TextView jTransformsLicense = findViewById(R.id.jTransformsLicense);
+        jTransformsLicense.setText(getString(R.string.license_header1) + " " + getString(R.string.jtransforms) + getString(R.string.license_header2)
+                + getString(R.string.jTranforms_license));
+
+        TextView iapLicense = findViewById(R.id.iapLicense);
+        iapLicense.setText(getString(R.string.license_header1) + " " + getString(R.string.gplay_iap_samples) + getString(R.string.license_header2)
+                + getString(R.string.iap_license));
+    }
+
 }
